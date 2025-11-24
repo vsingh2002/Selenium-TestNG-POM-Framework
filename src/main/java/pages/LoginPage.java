4d4a8e7fb58c30c37107e340607a6adc8f84ea00
unknown <vishalkumarsinghv60@gmail.com>
2025-11-24 15:30:12 +0530

    Adding Page Factory concept in the Login Page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,40 +1,53 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import utils.Log;
 
 public class LoginPage {
 	private WebDriver driver;
-	private By login = By.linkText("Log in");
-	private By emailField = By.id("Email");
-	private By passwordField = By.id("Password");
-	private By loginButton = By.xpath("//input[@value='Log in']");
+
+	@FindBy(linkText = "Log in")
+	WebElement loginLink;
+	@FindBy(id = "Email")
+	WebElement userEmailId;
+	@FindBy(id = "Password")
+	WebElement userPassword;
+	@FindBy(xpath = "//input[@value='Log in']")
+	WebElement loginButton;
 
 	public LoginPage(WebDriver driver) {
+
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 
 	}
 
 	public void clickLogin() {
 		Log.info("Clicking the login link");
-		driver.findElement(login).click();
+// driver.findElement(login).click();
+		loginLink.click();
 	}
 
 	public void enterEmail(String email) {
 		Log.info("Enterning the emailid");
-		driver.findElement(emailField).sendKeys(email);
+		// driver.findElement(emailField).sendKeys(email);
+		userEmailId.sendKeys(email);
 	}
 
 	public void enterPassword(String password) {
 		Log.info("Entering the password");
-		driver.findElement(passwordField).sendKeys(password);
+		// driver.findElement(passwordField).sendKeys(password);
+		userPassword.sendKeys(password);
 	}
 
 	public void loginButton() {
 		Log.info("Clicking login button");
-		driver.findElement(loginButton).click();
+		// driver.findElement(loginButton).click();
+		loginButton.click();
 
 	}
 }
